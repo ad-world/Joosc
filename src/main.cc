30e587cfeacca4a399516ea9afb8003c61d7e6ea
Amrit Sehgal <AmritGithub@gmail.com>
2024-01-26 15:40:55 -0500

    Add ParseTreeNode unit tests and remove factorial placeholder

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,7 @@
 #include <iostream>
-#include "factorial/factorial.h"
 #include "parsing/parseuser.h"
 
 int main(int argc, char *argv[]) {
-    std::cout << factorial(3) << std::endl;
     std::cout << "Hello world" << std::endl;
 
     return parsemain(argc, argv);
