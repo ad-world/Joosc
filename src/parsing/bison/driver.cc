0f92bfc7d912c1dffbf6bfc2f935c66155ab1146
Jagvir Dehal <jdehal@uwaterloo.ca>
2024-02-02 04:14:56 -0500

    Added graph visualation macro

--- src/parsing/bison/driver.cc
+++ src/parsing/bison/driver.cc
@@ -2,12 +2,24 @@
 #include "parser.hh"
 #include "../../ast/ast.h"
 #include <deque>
+#include <fstream>
 #include <stdexcept>
+
+// #define GRAPHVIZ
+#define GET_NAME(symbol) \
+    (((symbol) == yy::parser::symbol_kind::S_YYEMPTY) ? "YYEMPTY" \
+        : yy::parser::symbol_name( \
+            static_cast<yy::parser::symbol_kind_type>((symbol))))
 
 Driver::Driver()
 : trace_parsing (false), trace_scanning (false) {}
 
 int Driver::parse(const std::string &f) {
+#ifdef GRAPHVIZ
+    ofstream graph;
+    graph.open("graph.gv");
+#endif
+
     file = f;
     location.initialize(&file);
 
@@ -26,22 +38,41 @@ int Driver::parse(const std::string &f) {
     q.push_back(root);
     depth.push_back(0);
 
+#ifdef GRAPHVIZ
+    graph << "digraph G {\n\tgraph [layout=dot rankdir=LR]";
+#endif
+
     while ( !q.empty() ) {
         AstNode * curr = q.front();
         int curr_depth = depth.front();
 
-#ifdef PRINT_PARSETREE
+#ifdef PRINT_TREE
         for (int j = 0; j < curr_depth; j++) std::cout << '\t';
         std::cout << yy::parser::symbol_name(static_cast<yy::parser::symbol_kind_type>(curr->type)) << std::endl;
 #endif 
+
+#ifdef GRAPHVIZ
+        graph << "\n\t" << curr << " [label=\"" << GET_NAME(curr->type) << "\"]";
+        graph << "\n\t" << curr << " -> {";
+#endif
 
         q.pop_front();
         depth.pop_front();
         for ( auto child : curr->children ) {
             q.push_back(child);
             depth.push_back(curr_depth + 1);
+#ifdef GRAPHVIZ
+            graph << child << " ";
+        }
+        graph << "}";
+    }
+
+    graph << "\n}" << endl;
+    graph.close();
+#else
         }
     }
+#endif
 
     return result;
 }
