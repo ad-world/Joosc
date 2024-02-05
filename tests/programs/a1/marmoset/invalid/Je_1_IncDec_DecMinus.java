07ecddab4c5586bb61fb91f14ed76f62d6bcf731
Aryaman Dhingra <a23dhing@uwaterloo.ca>
2024-02-05 18:47:48 -0500

    Update testing directory structure

---
+++ tests/programs/a1/marmoset/invalid/Je_1_IncDec_DecMinus.java
@@ -0,0 +1,13 @@
+//PARSER_WEEDER
+//JOOS1:PARSER_EXCEPTION
+//JOOS2:PARSER_EXCEPTION
+
+public class Je_1_IncDec_DecMinus {
+	public Je_1_IncDec_DecMinus() {}
+	
+	public static int test() {
+		int i = 123;
+		return +++i;
+	}
+}
+
--- tests/src/integration/valid_invalid_prog_test.py
+++ tests/src/integration/valid_invalid_prog_test.py
@@ -27,7 +27,6 @@ def valid_invalid_prog_test():
     """
     integration_dir = os.path.dirname(__file__)
     programs_dir = os.path.join(integration_dir, "../../programs")
-
     valid_programs = os.path.join(programs_dir, "valid")
     invalid_programs = os.path.join(programs_dir, "invalid")
 
@@ -39,28 +38,35 @@ def valid_invalid_prog_test():
     print_pass_cases = True
     if len(sys.argv) > 1 and sys.argv[1] == "-f":
         print_pass_cases = False
+        failures = False
 
-    failures = False
-    for directory, expected_code in ((valid_programs, 0), (invalid_programs, 42)):
+    for assignment in os.listdir(programs_dir):
+        assignment_path = os.path.join(programs_dir, assignment)
+        local_valid_tests = os.path.join(assignment_path, "local/valid")
+        local_invalid_tests = os.path.join(assignment_path, "local/invalid")
 
-        header_bold_underline = colors.HEADER + colors.BOLD + colors.UNDERLINE
-        print(f"\n{header_bold_underline}Testing all programs in {resolve_path(directory, '')}:{colors.ENDC}")
+        marmo_valid_tests = os.path.join(assignment_path, "marmoset/valid")
+        marmo_invalid_tests = os.path.join(assignment_path, "marmoset/invalid")
 
-        for program in os.listdir(directory):
+        for directory, expected_code in ((local_valid_tests, 0), (local_invalid_tests, 42), (marmo_valid_tests, 0), (marmo_invalid_tests, 42)):
+            header_bold_underline = colors.HEADER + colors.BOLD + colors.UNDERLINE
+            print(f"\n{header_bold_underline}Testing all programs in {resolve_path(directory, '')}:{colors.ENDC}")
 
-            program_path = resolve_path(directory, program)
-            with open(integration_log_file, "w") as outfile:
-                result = subprocess.run([joosc_executable, program_path], stderr=outfile)
+            for program in os.listdir(directory):
 
-            if result.returncode == expected_code:
-                if print_pass_cases: # Test passed, display output if -f is not set
-                    print(f"{colors.OKGREEN}SUCCESS: Running joosc on {program} successfully returned {expected_code}{colors.ENDC}")
+                program_path = resolve_path(directory, program)
+                with open(integration_log_file, "w") as outfile:
+                    result = subprocess.run([joosc_executable, program_path], stderr=outfile)
+
+                if result.returncode == expected_code:
+                    if print_pass_cases: # Test passed, display output if -f is not set
+                        print(f"{colors.OKGREEN}SUCCESS: Running joosc on {program} successfully returned {expected_code}{colors.ENDC}")
+                        print_file_contents(integration_log_file)
+                else:
+                    print(f"{colors.FAIL}FAIL: Running joosc on {program} returned {result.returncode}. Expected: {expected_code}{colors.ENDC}")
                     print_file_contents(integration_log_file)
-            else:
-                print(f"{colors.FAIL}FAIL: Running joosc on {program} returned {result.returncode}. Expected: {expected_code}{colors.ENDC}")
-                print_file_contents(integration_log_file)
-                failures = True
-    
+                    failures = True
+        
     if failures:
         print(f"{colors.FAIL}\nERROR: Tests had failures.{colors.ENDC}")
         return 1
