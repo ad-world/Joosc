5c08b2b4d56cb8674b2ab10a0d1906183640c2e9
Amrit Sehgal <AmritGithub@gmail.com>
2024-02-10 21:46:37 -0500

    Change AstNode variant to use all the node types

--- src/variant-ast/astnode.h
+++ src/variant-ast/astnode.h
@@ -11,5 +11,44 @@
 // Include this header to include all AST classes
 
 typedef std::variant<
-    CompilationUnit
+    // Packages
+    CompilationUnit,
+    // Names
+    QualifiedIdentifier,
+    Identifier,
+    // Types
+    Type,
+    NonArrayType,
+    PrimitiveType,
+    // Classes
+    ClassDeclaration,
+    InterfaceDeclaration,
+    FieldDeclaration,
+    MethodDeclaration,
+    VariableDeclarator,
+    FormalParameter,
+    Modifier,
+    // Statements
+    Statement,
+    ExpressionStatement,
+    LocalVariableDeclaration,
+    Block,
+    IfThenStatement,
+    IfThenElseStatement,
+    WhileStatement,
+    ForStatement,
+    ReturnStatement,
+    // Expressions
+    Expression,
+    InfixExpression,
+    PrefixExpression,
+    CastExpression,
+    Assignment,
+    QualifiedThis,
+    ArrayCreationExpression,
+    Literal,
+    ClassInstanceCreationExpression,
+    FieldAccess,
+    ArrayAccess,
+    MethodInvocation
 > AstNodeVariant;
