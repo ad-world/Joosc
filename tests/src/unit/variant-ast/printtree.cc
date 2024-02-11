821972acfc67890985c4acf508bbc142283a648e
Amrit Sehgal <AmritGithub@gmail.com>
2024-02-10 21:52:01 -0500

    Change stub unit test

--- tests/src/unit/variant-ast/printtree.cc
+++ tests/src/unit/variant-ast/printtree.cc
@@ -1,29 +1,21 @@
-// #include "variant-ast/ast.h"
-// #include "variant-ast/visitor.h"
+#include "variant-ast/astnode.h"
 #include <memory>
+#include <vector>
 #include <gtest/gtest.h>
 
-// #include "variant-ast/statements.h"
 
-// std::unique_ptr<Variant::AstNode> make(Variant::AstNode &&node) {
-//   return std::make_unique<Variant::AstNode>(std::move(node));
-// }
+TEST(AstNode, ConstructorCompiles) {
+    auto package_declaration = std::unique_ptr<QualifiedIdentifier>();
+    auto single_imports = std::vector<QualifiedIdentifier>();
+    auto asterisk_imports = std::vector<QualifiedIdentifier>();
+    auto class_declarations = std::vector<ClassDeclaration>();
+    auto interface_declarations = std::vector<InterfaceDeclaration>();
 
-TEST(VariantAST, print) {
-    // // Main code: we would add constructors for a bit less pointer wrangling
-
-    // auto exp_stmt = Variant::ExpressionStatement();
-    // exp_stmt.statement_expression = make(Variant::Integer(5));
-    // auto then_stmt = std::make_unique<Variant::AstNode>(std::move(exp_stmt));
-
-    // auto if_stmt = Variant::IfStatement();
-    // if_stmt.if_clause = make(Variant::Integer(3));
-    // if_stmt.then_clause = std::move(then_stmt);
-
-    // auto program = Variant::CompilationUnit();
-    // program.program_statements.push_back(std::move(if_stmt));
-    // Variant::AstNode ast = std::move(program);
-
-    // auto visitor = Variant::PrintVisitor();
-    // visitor.visit(ast);
+    auto root = CompilationUnit(
+        package_declaration, 
+        single_imports, 
+        asterisk_imports, 
+        class_declarations, 
+        interface_declarations
+    );
 }
