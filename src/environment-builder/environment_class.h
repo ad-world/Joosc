6f3c547063c317ce0df63d073f2d9c2be57cadfe
Paranjay Agarwal <p38agarw@uwaterloo.ca>
2024-02-14 11:17:21 -0500

    added extra functionality for class/interface/method

--- src/environment-builder/environment_class.h
+++ src/environment-builder/environment_class.h
@@ -2,9 +2,24 @@
 #include <string>
 #include <memory>
 #include <optional>
+#include <vector>
 #include <variant>
 
-typedef std::variant<bool, int, char, std::byte, short, std::string> VariableType;
+enum PackageType {
+    CLASS,
+    INTERFACE
+};
+
+typedef std::variant<std::monostate, bool, int64_t, char, std::string, PackageType> VariableType;
+
+enum Modifier {
+    PUBLIC,
+    PROTECTED,
+    ABSTRACT,
+    STATIC,
+    NATIVE,
+    FINAL
+};
 
 struct Variable {
     std::string type;
@@ -14,27 +29,53 @@ struct Variable {
 
 class RootEnvironment {
     protected:
-        std::unique_ptr<RootEnvironment> parent;
+        std::unique_ptr<RootEnvironment> parent = nullptr;
         std::unordered_map<std::string, Variable> variables;
     public:
-        RootEnvironment(std::unique_ptr<RootEnvironment> parent = nullptr);
+        RootEnvironment(std::unique_ptr<RootEnvironment>& parent);
         // add variable to environment
         void addVariable(const std::string& name, const Variable& variable);
         // lookup a variable in that environment and parent environments
         std::optional<Variable> lookupVariable(const std::string& name);
 };
 
-class ClassEnvironment: public RootEnvironment {
+class PackageEnvironment: public RootEnvironment {
     public:
-        ClassEnvironment(std::unique_ptr<RootEnvironment> parent);
+        PackageEnvironment(std::unique_ptr<RootEnvironment>& parent);
+};
+
+class ClassEnvironment: public RootEnvironment {
+    std::vector<Modifier> modifiers;
+    public:
+        ClassEnvironment(
+            std::unique_ptr<RootEnvironment>& parent,
+            std::vector<Modifier>& modifier
+        );
+};
+
+class InterfaceEnvironment: public RootEnvironment {
+    std::vector<Modifier> modifiers;
+    public:
+        InterfaceEnvironment(
+            std::unique_ptr<RootEnvironment>& parent,
+            std::vector<Modifier>& modifiers
+        );
 };
 
 class MethodEnvironment: public RootEnvironment {
+    std::vector<Modifier> modifiers;
+    std::string name;
+    std::string return_type;
     public:
-        MethodEnvironment(std::unique_ptr<RootEnvironment> parent);
+        MethodEnvironment(
+            std::unique_ptr<RootEnvironment>& parent,
+            std::vector<Modifier>& modifiers,
+            std::string& name,
+            std::string& return_type
+        );
 };
 
 class BlockEnvironment: public RootEnvironment {
     public:
-        BlockEnvironment(std::unique_ptr<RootEnvironment> parent);
+        BlockEnvironment(std::unique_ptr<RootEnvironment>& parent);
 };
