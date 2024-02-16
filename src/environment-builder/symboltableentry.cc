92154cf9267d218594a282359b47435e24528bc5
Amrit Sehgal <AmritGithub@gmail.com>
2024-02-16 11:54:15 -0500

    Fix segmentation fault due to constructors not building tables

--- src/environment-builder/symboltableentry.cc
+++ src/environment-builder/symboltableentry.cc
@@ -1,23 +1,35 @@
 #include "symboltableentry.h"
 #include "symboltable.h"
 
+std::unique_ptr<SymbolTable> init_table() {
+    return std::make_unique<SymbolTable>();
+}
+
 PackageDeclarationObject::PackageDeclarationObject(const std::string &identifier) : 
-    identifier{identifier}, is_default_package{false} {}
+    identifier{identifier},
+    is_default_package{false},
+    sub_packages{init_table()},
+    classes{init_table()},
+    interfaces{init_table()} {}
 
 PackageDeclarationObject::PackageDeclarationObject() : 
-    identifier{""}, is_default_package{true} {}
+    identifier{""},
+    is_default_package{true},
+    sub_packages{init_table()},
+    classes{init_table()},
+    interfaces{init_table()} {}
 
 ClassDeclarationObject::ClassDeclarationObject(const std::string &identifier) :
-    identifier{identifier}, fields{std::make_unique<SymbolTable>()}, methods{} {}
+    identifier{identifier}, fields{init_table()}, methods{init_table()} {}
 
 InterfaceDeclarationObject::InterfaceDeclarationObject(const std::string &identifier) :
-    identifier{identifier} {}
+    identifier{identifier}, methods{init_table()} {}
 
 FieldDeclarationObject::FieldDeclarationObject(const std::string &identifier) :
     identifier{identifier} {}
 
 MethodDeclarationObject::MethodDeclarationObject(const std::string &identifier) :
-    identifier{identifier} {}
+    identifier{identifier}, parameters{init_table()}, local_variables{init_table()} {}
 
 FormalParameterDeclarationObject::FormalParameterDeclarationObject(const std::string &identifier) :
     identifier{identifier} {}
