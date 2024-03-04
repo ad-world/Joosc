a2a616887471a0b07a41597f710135a2077be3fc
Jagvir Dehal <jdehal@uwaterloo.ca>
2024-03-04 12:50:41 -0500

    Merge origin/main into branch

--- src/parsing/bison/driver.cc
+++ src/parsing/bison/driver.cc
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <stdexcept>
 
-// #define GRAPHVIZ
 #define GET_NAME(symbol) \
     (((symbol) == yy::parser::symbol_kind::S_YYEMPTY) ? "YYEMPTY" \
         : yy::parser::symbol_name( \
@@ -14,11 +13,6 @@ Driver::Driver()
 : trace_parsing (false), trace_scanning (false) {}
 
 int Driver::parse(const std::string &f) {
-#ifdef GRAPHVIZ
-    ofstream graph;
-    graph.open("graph.gv");
-#endif
-
     file = f;
     location.initialize(&file);
 
@@ -33,61 +27,3 @@ int Driver::parse(const std::string &f) {
 
     return result;
 }
-
-#if 0
-
-    // Traverse and print parse tree
-    deque<AstNode *> q;
-    deque<int> depth;
-    q.push_back(root);
-    depth.push_back(0);
-
-#ifdef GRAPHVIZ
-    graph << "digraph G {\n\tgraph [layout=dot rankdir=LR]";
-#endif
-
-    while ( !q.empty() ) {
-        AstNode * curr = q.front();
-        int curr_depth = depth.front();
-
-#ifdef PRINT_TREE
-        for (int j = 0; j < curr_depth; j++) std::cout << '\t';
-        std::cout << yy::parser::symbol_name(static_cast<yy::parser::symbol_kind_type>(curr->type)) << std::endl;
-#endif 
-
-#ifdef GRAPHVIZ
-        graph << "\n\t\"" << curr << "\" [label=\"" << GET_NAME(curr->type);
-        if ( curr->value.has_value() ) {
-            auto & innerValue = curr->value.value();
-            graph << "\\n";
-            if ( holds_alternative<string>(innerValue) ) {
-                graph << get<string>(innerValue);
-            } else if ( holds_alternative<long int>(innerValue) ) {
-                graph << get<long int>(innerValue);
-            }
-        }
-        graph << "\"]";
-        graph << "\n\t\"" << curr << "\" -> {";
-#endif
-
-        q.pop_front();
-        depth.pop_front();
-        for ( auto child : curr->children ) {
-            q.push_back(child);
-            depth.push_back(curr_depth + 1);
-#ifdef GRAPHVIZ
-            graph << "\"" << child << "\" ";
-        }
-        graph << "}";
-    }
-
-    graph << "\n}" << endl;
-    graph.close();
-#else
-        }
-    }
-#endif
-
-    return result;
-}
-#endif
