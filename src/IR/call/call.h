8c549457bc53e38da019ded7af90809bc77e551e
Jagvir Dehal <jdehal@uwaterloo.ca>
2024-03-21 15:06:08 -0400

    Add constructors back to each function

--- src/IR/call/call.h
+++ src/IR/call/call.h
@@ -10,8 +10,8 @@ protected:
     std::unique_ptr<ExpressionIR> target;
     std::vector<std::unique_ptr<ExpressionIR>> args;
 public:
-    CallIR(std::unique_ptr<ExpressionIR> target, std::vector<std::unique_ptr<ExpressionIR> > args);
-        // : target{std::move(target)}, args(args) {}
+    CallIR(std::unique_ptr<ExpressionIR> target, std::vector<std::unique_ptr<ExpressionIR> > args)
+        : target{std::move(target)}, args{std::move(args)} {}
     ExpressionIR &getTarget() { assert(target.get()); return *target.get(); };
     std::vector<std::unique_ptr<ExpressionIR>> &getArgs() { return args; };
     int getNumArgs() { return args.size(); };
--- src/IR/eseq/eseq.h
+++ src/IR/eseq/eseq.h
@@ -10,8 +10,8 @@ class ESeqIR {
     std::unique_ptr<ExpressionIR> expr;
 
 public:
-    ESeqIR(std::unique_ptr<StatementIR> stmt, std::unique_ptr<ExpressionIR> expr);
-        // : stmt(std::move(stmt)), expr(std::move(expr)) {}
+    ESeqIR(std::unique_ptr<StatementIR> stmt, std::unique_ptr<ExpressionIR> expr)
+        : stmt(std::move(stmt)), expr(std::move(expr)) {}
     StatementIR &getStmt() { assert(stmt.get()); return *stmt.get(); }
     ExpressionIR &getExpr() { assert(expr.get()); return *expr.get(); }
     std::string label() { return "ESEQ"; }
