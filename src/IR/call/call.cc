1766ae2787fe7fefb0b79e18deb046e80054d3b2
Jagvir Dehal <jdehal@uwaterloo.ca>
2024-03-23 14:33:14 -0400

    Helpers: Bugfix

--- src/IR/call/call.cc
+++ src/IR/call/call.cc
@@ -6,12 +6,12 @@
 std::unique_ptr<ExpressionIR> CallIR::makeMalloc(std::unique_ptr<ExpressionIR> arg) {
     std::vector<std::unique_ptr<ExpressionIR>> args(1); // one vector
     args.push_back(std::move(arg));
-    return makeExpr(NameIR::makeMalloc(), args);
+    return makeExpr(NameIR::makeMalloc(), std::move(args));
 }
 
 std::unique_ptr<ExpressionIR> CallIR::makeException() {
     std::vector<std::unique_ptr<ExpressionIR>> args(0); // empty vector
-    return makeExpr(NameIR::makeException(), args);
+    return makeExpr(NameIR::makeException(), std::move(args));
 }
 
 std::unique_ptr<ExpressionIR> CallIR::makeExpr(std::unique_ptr<ExpressionIR> target, std::vector<std::unique_ptr<ExpressionIR> > args) {
