e52bf445b6d735f2424d288ea5be68ca3f7b657e
Amrit Sehgal <AmritGithub@gmail.com>
2024-03-25 13:21:17 -0400

    Setup changes

--- src/IR-canonicalizer/ir-canonicalizer.cc
+++ src/IR-canonicalizer/ir-canonicalizer.cc
@@ -1,42 +1,61 @@
 #include "ir-canonicalizer.h"
 #include "utillities/util.h"
-#include "IR/ir.h"
 #include "exceptions/exceptions.h"
 
+IRCanonicalizer::LoweredStatement::LoweredStatement(std::vector<StatementIR> statements)
+    : statements{std::move(statements)} {}
+
+IRCanonicalizer::LoweredExpression::LoweredExpression(
+        std::vector<StatementIR> statements, std::unique_ptr<ExpressionIR> expression
+    ) : statements{std::move(statements)}, expression{std::move(expression)} {}
+
+IRCanonicalizer::LoweredExpression::LoweredExpression(
+        std::vector<StatementIR> statements, ExpressionIR expression
+    ) : statements{std::move(statements)}, expression{std::make_unique<ExpressionIR>(std::move(expression))} {}
+
 void IRCanonicalizer::operator()(IR &ir) { convert(ir); }
-void IRCanonicalizer::operator()(ExpressionIR &ir) { convert(ir); }
-void IRCanonicalizer::operator()(StatementIR &ir) { convert(ir); }
+
+IRCanonicalizer::LoweredExpression IRCanonicalizer::operator()(ExpressionIR &ir) { return convert(ir); }
+IRCanonicalizer::LoweredStatement IRCanonicalizer::operator()(StatementIR &ir) { return convert(ir); }
 
 void IRCanonicalizer::convert(IR &ir) {
     std::visit(util::overload {
         [&](CompUnitIR &node) {},
         [&](FuncDeclIR &node) {},
-        [&](ExpressionIR &node) { return convert(node); },
-        [&](StatementIR &node) { return convert(node); }
+        [&](ExpressionIR &node) { convert(node); },
+        [&](StatementIR &node) { convert(node); }
     }, ir);
 }
 
-void IRCanonicalizer::convert(ExpressionIR &ir) {
-    std::visit(util::overload {
-        [&](BinOpIR &node) {},
-        [&](CallIR &node) {},
-        [&](ConstIR &node) { return; },
-        [&](ESeqIR &node) {},
-        [&](MemIR &node) {},
-        [&](NameIR &node) { return; },
-        [&](TempIR &node) { return; }
+IRCanonicalizer::LoweredExpression IRCanonicalizer::convert(ExpressionIR &ir) {
+    return std::visit(util::overload {
+        // [&](BinOpIR &node) {},
+        // [&](CallIR &node) {},
+        [&](ConstIR &node) { return IRCanonicalizer::LoweredExpression(std::move(node)); },
+        // [&](ESeqIR &node) {},
+        // [&](MemIR &node) {},
+        [&](NameIR &node) { return IRCanonicalizer::LoweredExpression(std::move(node)); },
+        [&](TempIR &node) { return IRCanonicalizer::LoweredExpression(std::move(node)); },
+
+        // TEMPORARILY HERE WHILE WIP
+        [&](auto &node) { return IRCanonicalizer::LoweredExpression(std::move(node)); }
     }, ir);
 }
 
-void IRCanonicalizer::convert(StatementIR &ir) {
-    std::visit(util::overload {
-        [&](CJumpIR &node) {},
-        [&](ExpIR &node) {},
-        [&](JumpIR &node) {},
-        [&](LabelIR &node) { return; },
-        [&](MoveIR &node) {},
-        [&](ReturnIR &node) {},
-        [&](SeqIR &node) {},
-        [&](CallIR &node) { THROW_CompilerError("Call IR should not be considered a statement before conversion"); }
+IRCanonicalizer::LoweredStatement IRCanonicalizer::convert(StatementIR &ir) {
+    return std::visit(util::overload {
+        // [&](CJumpIR &node) {return {};},
+        // [&](ExpIR &node) {return {};},
+        // [&](JumpIR &node) {return {};},
+        [&](LabelIR &node) { return IRCanonicalizer::LoweredStatement(std::move(node)); },
+        // [&](MoveIR &node) {return {};},
+        // [&](ReturnIR &node) {return {};},
+        // [&](SeqIR &node) {return {};},
+        [&](CallIR &node) -> IRCanonicalizer::LoweredStatement {
+             THROW_CompilerError("Call IR should not be considered a statement before conversion"); 
+        },
+
+        // TEMPORARILY HERE WHILE WIP
+        [&](auto &node) { return IRCanonicalizer::LoweredStatement(std::move(node)); }
     }, ir);
 }
--- src/IR-canonicalizer/ir-canonicalizer.h
+++ src/IR-canonicalizer/ir-canonicalizer.h
@@ -1,15 +1,45 @@
 #pragma once
 
-#include "IR/ir_variant.h"
+#include "IR/ir.h"
+#include <vector>
+#include <memory>
 
 // Convert IR to Canonical IR form
 class IRCanonicalizer {
+    struct LoweredStatement {
+        std::vector<StatementIR> statements;
+
+        LoweredStatement() = default; 
+        LoweredStatement(std::vector<StatementIR> statements);
+
+        template<typename... StatementIRs> LoweredStatement(StatementIRs&&... args) {
+            (statements.emplace_back(std::forward<StatementIRs>(args)), ...);
+        }
+    };
+
+    struct LoweredExpression {
+        std::vector<StatementIR> statements;
+        std::unique_ptr<ExpressionIR> expression;
+
+        LoweredExpression() = default;
+        LoweredExpression(std::vector<StatementIR> statements, std::unique_ptr<ExpressionIR> expression);
+        LoweredExpression(std::vector<StatementIR> statements, ExpressionIR expression);
+
+        template<typename... StatementIRs> LoweredExpression(ExpressionIR expression, StatementIRs&&... args) {
+            this->expression = std::make_unique<ExpressionIR>(std::move(expression));
+            (statements.emplace_back(std::forward<StatementIRs>(args)), ...);
+        }
+    };
+
   public:
     void operator()(IR&);
-    void operator()(ExpressionIR&);
-    void operator()(StatementIR&);
-
     void convert(IR&);
-    void convert(ExpressionIR&);
-    void convert(StatementIR&);
+
+    // Return lowered version of the expression
+    LoweredExpression operator()(ExpressionIR&);
+    LoweredExpression convert(ExpressionIR&);
+
+    // Return lowered version of the statement
+    LoweredStatement operator()(StatementIR&);
+    LoweredStatement convert(StatementIR&);
 };
