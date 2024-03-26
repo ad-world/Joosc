3811f90ccdee505037891ee27da274a9143a1272
Amrit Sehgal <AmritGithub@gmail.com>
2024-03-26 10:54:59 -0400

    Add more concatenate helpers just in case

--- src/IR-canonicalizer/concatenate.h
+++ src/IR-canonicalizer/concatenate.h
@@ -7,6 +7,10 @@ std::vector<StatementIR> concatenate(std::vector<StatementIR>& first) {
     return std::move(first);
 }
 
+std::vector<StatementIR> concatenate(std::vector<StatementIR>&& first) {
+    return std::move(first);
+}
+
 template<typename ItemType>
 std::vector<StatementIR> concatenate(ItemType&& first) {
     auto vec = std::vector<StatementIR>();
@@ -29,6 +33,14 @@ std::vector<StatementIR> concatenate(ItemType&& first, Ts&&... args) {
 
 template<typename ItemType, typename... Ts>
 std::vector<StatementIR> concatenate(std::vector<ItemType>& first, Ts&&... args) {
+    for (auto& elem : concatenate(args...)) {
+        first.emplace_back(std::move(elem));
+    }
+    return std::move(first);
+}
+
+template<typename ItemType, typename... Ts>
+std::vector<StatementIR> concatenate(std::vector<ItemType>&& first, Ts&&... args) {
     for (auto& elem : concatenate(args...)) {
         first.emplace_back(std::move(elem));
     }
