d416bb93861d26a9619f9f03b38e6a4a74542a8c
Jagvir Dehal <jagvir.dehal@uwaterloo.ca>
2024-03-29 12:39:37 -0400

    Update main.cc stupid merge

--- src/main.cc
+++ src/main.cc
@@ -39,12 +39,6 @@ int main(int argc, char *argv[]) {
                 case static_cast<char>(CommandLineArg::RUN_AND_TEST_IR):
                     compiler.setRunIR(true);
                     break;
-                case static_cast<char>(CommandLineArg::STATIC_ANALYSIS_ONLY):
-                    emit_code = false;
-                    break;
-                case static_cast<char>(CommandLineArg::RUN_AND_TEST_IR):
-                    run_ir = true;
-                    break;
                 default:
                     throw cmd_error();
             }
