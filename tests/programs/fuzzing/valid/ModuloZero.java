eddc288a6845d8c717929d384cdc68cbadab0239
Jagvir Dehal <jdehal@uwaterloo.ca>
2024-04-03 23:40:44 -0400

    Add ModuloZero test

---
+++ tests/programs/fuzzing/valid/ModuloZero.java
@@ -0,0 +1,18 @@
+// The compiler should not attempt to evaluate the constant 0 % 0
+public class ModuloZero {
+	public static int[] t = new int[100];
+	public ModuloZero() {
+	}
+	public static void test() {
+	}
+	public static void m0() {
+		while (((((0)%(0))<(0))&&(false))){
+		}
+	}
+	public static void m1() {
+	}
+	public static void m2() {
+	}
+	public static void m3() {
+	}
+}
