f17ebab785ea8341cbf0c73d90dc419d0d2496ed
Amrit Sehgal <AmritGithub@gmail.com>
2024-04-05 17:54:18 -0400

    Fixes

--- java-ir/ir/interpret/Configuration.java
+++ java-ir/ir/interpret/Configuration.java
@@ -7,7 +7,7 @@ public class Configuration {
     /* Some special stack-related names that are used in the IR */
     /** Prefix for argument registers */
     public static final String ABSTRACT_ARG_PREFIX = "_ARG";
-    public static final String ABSTRACT_RET = "_RET";
+    public static final String ABSTRACT_RET = "RET";
 
     /** Word size; assumes a 32-bit architecture */
     public static final int WORD_SIZE = 4;
