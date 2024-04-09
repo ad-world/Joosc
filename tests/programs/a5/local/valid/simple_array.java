de68e305afd062a59a51fd73bebc1cdc9c6cbab5
Aryaman Dhingra <a23dhing@uwaterloo.ca>
2024-04-08 23:15:35 -0400

    Add simple array test

---
+++ tests/programs/a5/local/valid/simple_array.java
@@ -0,0 +1,12 @@
+// CODE_GENERATION
+public class simple_array {
+    public simple_array() {}
+    public static int test() {
+    int[] a = new int[100]; 
+    
+    int[] x = a;
+
+    x[16] = 123;
+    return a[48];
+   }
+}
