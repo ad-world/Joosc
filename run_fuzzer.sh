1dacac0edd3842388a01cd6201b4e827aa98e2df
Jagvir Dehal <jdehal@uwaterloo.ca>
2024-04-16 13:34:28 -0400

    Changes to fuzz docker build

--- run_fuzzer.sh
+++ run_fuzzer.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-DOCKER=$(docker --version 2>/dev/null >/dev/null && echo "docker" || echo "podman")
+DOCKER=docker #$(docker --version 2>/dev/null >/dev/null && echo "docker" || echo "podman")
 IMAGE=fuzzer
+CCACHE=ccache
 
 set -e
 make clean
 $DOCKER build -t $IMAGE -f fuzz/Dockerfile .
-$DOCKER run --rm -it $IMAGE
+$DOCKER volume create $CCACHE --ignore
+$DOCKER run --name $IMAGE --replace -v "$CCACHE:/ccache" -it $IMAGE
